2b2cac55df78f4d794def2989e04f6ef6797b152
aguirrehdezma <aguirrehdezma@gmail.com>
2025-02-14 09:41:23 -0700

    feat: Add products lists

--- app/src/main/java/mx/edu/itesca/practica3/MenuActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica3/MenuActivity.kt
@@ -1,20 +1,58 @@
 package mx.edu.itesca.practica3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnAntojitos: Button = findViewById(R.id.antojitos)
+        var btnEspecialidades: Button = findViewById(R.id.especialidades)
+        var btnCombinations: Button = findViewById(R.id.combinations)
+        var btnTortas: Button = findViewById(R.id.tortas)
+        var btnSopas: Button = findViewById(R.id.sopas)
+        var btnDrinks: Button = findViewById(R.id.drinks)
+
+        btnAntojitos.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Antojitos")
+            startActivity(intent)
+        }
+
+        btnEspecialidades.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Especialidades")
+            startActivity(intent)
+        }
+
+        btnCombinations.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Combinations")
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Tortas")
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Sopas")
+            startActivity(intent)
+        }
+
+        btnDrinks.setOnClickListener {
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Drinks")
+            startActivity(intent)
         }
     }
 }
